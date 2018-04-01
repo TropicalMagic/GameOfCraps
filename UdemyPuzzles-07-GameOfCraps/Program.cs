1a526c3b9e4b204839fd2e84acd21228d5806b7c
TROPICAL MAGIC <pageup500@gmail.com>
2018-04-01 11:49:57 +0800

    Update

---
+++ UdemyPuzzles-07-GameOfCraps/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyPuzzles_07_GameOfCraps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CrapsGame craps = new CrapsGame();
+            craps.Play();
+        }
+    }
+}
